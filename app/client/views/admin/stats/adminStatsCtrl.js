51c2b9d721153d740aad9710a42cc07b01513668
littleguga <fed777os@gmail.com>
2018-04-12 02:38:44 +0300

    add draft of saving txt

--- app/client/views/admin/stats/adminStatsCtrl.js
+++ app/client/views/admin/stats/adminStatsCtrl.js
@@ -1,21 +1,52 @@
 angular.module('reg')
-  .controller('AdminStatsCtrl',[
+  .controller('AdminStatsCtrl', [
     '$scope',
     'UserService',
-    function($scope, UserService){
+    function ($scope, UserService) {
 
       $('.ui.accordion')
-        .accordion()
-      ;
+        .accordion();
+
+
+      function saveThisTable(el) {
+        var fileText = "";
+        var fileTitle = el.previousElementSibling.textContent;
+        var tableBody = el.nextElementSibling.querySelectorAll('tbody')[0];
+        var tableRows = tableBody.querySelectorAll('tr');
+        var columnsAmount = tableRows[0].children.length;
+        var rowsAmount = tableRows.length;
+
+        for (var i = 0; i < rowsAmount; i++) {
+          for (var j = 0; j < columnsAmount; j++) {
+
+            fileText += tableRows[i].children[j].textContent + ' ';
+          }
+
+          fileText += '\n';
+        }
+
+        downloadTextFile(fileText, fileTitle);
+      }
+
+      function downloadTextFile(fileText, fileTitle) {
+        var text = fileText,
+          blob = new Blob([text], {type: 'text/plain'}),
+          anchor = document.createElement('a');
+
+        anchor.download = fileTitle + ".txt";
+        anchor.href = (window.webkitURL || window.URL).createObjectURL(blob);
+        anchor.dataset.downloadurl = ['text/plain', anchor.download, anchor.href].join(':');
+        anchor.click();
+      }
 
       UserService
         .getStats()
-        .success(function(stats){
+        .success(function (stats) {
           $scope.stats = stats;
           $scope.loading = false;
         });
 
-      $scope.fromNow = function(date){
+      $scope.fromNow = function (date) {
         return moment(date).fromNow();
       };
 
